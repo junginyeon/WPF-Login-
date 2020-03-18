1d0cc14bde94a572bf4e6e1970b297241e39d6b5
yeonjungin <0206dus@gmail.com>
2020-03-19 00:30:19 +0900

    0318 마지막작업(로그인버튼 안눌림)

--- WPF_Login/WPF_Login/View/Main.xaml.cs
+++ WPF_Login/WPF_Login/View/Main.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WPF_Login.ViewModel;
 
 namespace WPF_Login.View
 {
@@ -22,6 +23,7 @@ namespace WPF_Login.View
         public Main()
         {
             InitializeComponent();
+            this.DataContext = new LoginViewModel();
         }
     }
 }
--- WPF_Login/WPF_Login/ViewModel/LoginViewModel.cs
+++ WPF_Login/WPF_Login/ViewModel/LoginViewModel.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_Login.BL;
+using WPF_Login.View;
+using System.Windows;
 
 namespace WPF_Login.ViewModel
 {
     class LoginViewModel
     {
+        public string Id { get; set; }
+        public string Pwd { get; set; }
+        public Command LoginCommand { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(executeLogin, canexecuteLogin);
+        }
+        private void executeLogin(object obj)
+        {
+            if(Id=="root" && Pwd == "1234")
+            {
+                Login page = new Login();
+                page.ShowDialog();
+            }
+        }
+        private bool canexecuteLogin(object arg)
+        {
+            return true;
+        }
     }
 }
